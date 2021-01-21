8648ff7fa5204f7c9fa1194e02e111ade1890844
Enoragbon David <enoragbondavid35@gmail.com>
2021-01-21 10:25:27 +0100

    first commit

---
+++ src/data/config.js
@@ -0,0 +1,10 @@
+
+module.exports = {
+  // TODO: change your profile information here
+  name: "David Enoragbon",
+  greeting: "Hey 👋",
+  greetingDescription: "I'm David Enoragbon and I'm a Software Engineer!",
+  githubUrl: "https://github.com/maximdave",
+  linkedinUrl: "https://linkedin.com/in/davidenoragbon",
+  cvLink: "https://docs.google.com/document/d/1E1234561NMyQA67890ygda7abcdefghijs1jBp7HFI0/",
+};
